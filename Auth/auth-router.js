1f1cc6f0179bf51b5518f9b58c858bfdd3ed8ed0
Wangdi Tenzing <wangdi457@gmail.com>
2020-04-23 18:12:19 -0400

    added an endpoint to retreive the list of users

--- Auth/auth-router.js
+++ Auth/auth-router.js
@@ -31,8 +31,14 @@ router.post("/login", (req,res)=>{
    })
 })
 
-router.get("/register", (req, res) => {
-res.send("from router")
+router.get("/users", (req, res) => {
+Users.find()
+.then(users=>{
+  res.status(200).json(users)
+})
+.catch(err=>{
+    res.status(500).json({message:"Cannot find users", err})
+})
   });
 
 module.exports=router
